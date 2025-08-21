4c3bfe73921e7f0572eaaee20afd3a02110c8309
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2025-08-21 09:51:17 -0300

    ejercicio6

---
+++ ejercicio6/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ejercicio6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingresar una palabra y mostrar la cantidad de vocales que tiene.
+            
+            Console.WriteLine("Ingrese una palabra para saber cuantas vocales tiene");
+            string palabra = Console.ReadLine().ToLower();
+            int cantVocales = examinarvocal(palabra);
+            Console.WriteLine($"La cantiad de vocales que tiene la palabra :{palabra} es {cantVocales}");
+            Console.WriteLine("Enter para salir");
+            Console.ReadLine();
+        }
+
+        static int examinarvocal(string palabra)
+        {
+            int contador = 0;
+            for(int i =0; i<palabra.Length; i++)
+            {
+                if (palabra[i]== 'a' || palabra[i] == 'e' || palabra[i] =='i' || palabra[i]== 'o' || palabra[i]=='u')
+                {
+                    contador++;
+                }
+            }
+            return contador;
+        }
+    }
+}
