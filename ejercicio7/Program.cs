53f8d837fb68a4c121e420e5636be44b94c39f63
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2025-08-21 10:47:02 -0300

    ejercicio7

---
+++ ejercicio7/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ejercicio7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ingresar una palabra y mostrarla en el otro sentido(Hola -> aloH).
+
+            Console.WriteLine("Ingrese una palabra para invertir");
+            string palabra = Console.ReadLine();
+            string palabraInvertida = invertirPalabra(palabra);
+            Console.WriteLine($"La palabra {palabra} invertida es {palabraInvertida}");
+            Console.WriteLine("enter para salir");
+            Console.ReadLine();
+        }
+
+        static string invertirPalabra (string palabra)
+        {
+            string palabraInvertida = "";
+            for(int i= palabra.Length -1 ; i>=0; i--)
+            {
+                palabraInvertida += palabra[i];
+            }
+            return palabraInvertida; 
+        }
+    }
+}
